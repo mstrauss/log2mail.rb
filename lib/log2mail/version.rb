8bb3d3742ed3c7ae6a90935b4d367919385cf6b8
Markus Strauss <git@ms.sieb.mx>
2014-12-15 12:36:50 +0100

    version 0.0.1.pre2

--- lib/log2mail/version.rb
+++ lib/log2mail/version.rb
@@ -1,6 +1,6 @@
 module Log2mail
   PROGNAME    = 'log2mail.rb'
-  VERSION     = "0.0.1.pre1"
+  VERSION     = "0.0.1.pre2"
   AUTHOR      = "Markus Strauss"
   AUTHOR_MAIL = "log2mail@dev.sieb.mx"
 end
